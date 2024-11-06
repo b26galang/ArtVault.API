ebf883be432da778fd8e7a972a829326f27329db
b26galang <galanbri@gmail.com>
2024-11-05 19:21:55 -0800

    Add DTOs and endpoints for comment controller, update model ID properties, add AutoMapper

---
+++ ArtVault.API/DTOs/CommentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtVault.API.DTOs
+{
+    public class CommentDto
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Guid UserId { get; set; }
+        public Guid PostId { get; set; }
+    }
+}
